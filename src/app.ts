ed0b0d9483d38b600ca35dc2ff8419db7bb213ef
mohamed12samy <mohamedsamyabbas99@gmail.com>
2024-08-20 00:15:02 +0300

    fix

--- src/app.ts
+++ src/app.ts
@@ -26,15 +26,15 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  const allowedOrigins = ['membersofhumatalwatan.com'];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
+  const allowedOrigins :string[] = ['membersofhumatalwatan.com'];
+  const origin : string | undefined = req.headers.origin;
+  if (origin && allowedOrigins.includes(origin)) {
        res.setHeader('Access-Control-Allow-Origin', origin);
   }
   //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
   res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Credentials', "true");
   return next();
 });
 
